f4b9f92e88c6ea721d805c753685a42a1444888a
DeividasRo <99316667+DeividasRo@users.noreply.github.com>
2022-02-10 00:59:52 +0200

    Project files

--- IremintasPasveikinimas/main.cpp
+++ IremintasPasveikinimas/main.cpp
@@ -1,28 +1,43 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	string e1, e2, e3, e4, e5;
+struct Remelis {
+	string e1, e2, e3;
 	string vardas;
 	string pasisveikinimas;
-	cout << "Iveskite savo varda: ";
-	cin >> vardas;
-	if (vardas.back() == 's')
-		pasisveikinimas = "Sveikas, ";
-	else
-		pasisveikinimas = "Sveika, ";
-	e1 = string(vardas.length() + 13, '*');
-	e2 = "* " + string(vardas.length() + 9, ' ') + " *";
-	e3 = "* " + pasisveikinimas + vardas + " *";
-	e4 = "* " + string(vardas.length() + 9, ' ') + " *";
-	e5 = string(vardas.length() + 13, '*');
-	cout << e1 << endl;
-	cout << e2 << endl;
-	cout << e3 << endl;
-	cout << e4 << endl;
-	cout << e5 << endl;
+	int remelio_plotis;
+	void Nuskaitymas()
+	{
+		cout << "Iveskite savo varda: ";
+		cin >> vardas;
+		cout << "Iveskite remelio ploti: ";
+		cin >> remelio_plotis;
+		if (vardas.back() == 's')
+			pasisveikinimas = "Sveikas, ";
+		else
+			pasisveikinimas = "Sveika, ";
+		e1 = string(vardas.length() + remelio_plotis * 2 + pasisveikinimas.length() + 2, '*') + "\n";
+		e2 = "*" + string(vardas.length() + remelio_plotis * 2 + pasisveikinimas.length(), ' ') + "*\n";
+		e3 = "*" + string(remelio_plotis, ' ') + pasisveikinimas + vardas + string(remelio_plotis, ' ') + "*\n";
+	}
 
+	void Isvedimas()
+	{
+		cout << e1;
+		for (int i = 0; i < remelio_plotis; i++)
+			cout << e2;
+		cout << e3;
+		for (int i = 0; i < remelio_plotis; i++)
+			cout << e2;
+		cout << e1;
+	}
+};
+
+int main()
+{
+	Remelis r;
+	r.Nuskaitymas();
+	r.Isvedimas();
 	cin.get();
 	return 0;
 }
